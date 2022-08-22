9b3e9a2e6b53dbb8361c4c8d782339158590bfa1
Ilnaz <mmmr.sh.i@gmail.com>
2022-08-22 11:40:24 +0300

    Add font

--- src/services/registrationService/registrationService.container.tsx
+++ src/services/registrationService/registrationService.container.tsx
@@ -1,9 +1,6 @@
 import { useEvent, useStore } from "effector-react";
-import React, { FC } from "react";
 import { registrationService } from "./registrationService.model";
 import { RegistrationForm } from "./view/RegistrationForm";
-
-
 
 export const RegistrationContainer = () => {
    const handleRegisterUser = useEvent(
